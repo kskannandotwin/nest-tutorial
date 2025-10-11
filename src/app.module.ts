63ac40a660883e96ef6aaddd455b158b3bef2291
Jobulikannan <kannan@jobuli.com>
2025-10-12 00:12:35 +0530

    12.10.2025
    3-25-cma completed

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostModule } from './post/post.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [PostModule],
+  imports: [PostModule, MongooseModule.forRoot('mongodb+srv://kskannanwin_db_user:eord1cn0vnpe05g0@postcluster.cpgqqvt.mongodb.net/?retryWrites=true&w=majority&appName=PostCluster')],
   controllers: [AppController],
   providers: [AppService],
 })
