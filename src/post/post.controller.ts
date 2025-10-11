fbc333876d3dc6022ba4d8c3964b6de713ecd9ad
Jobulikannan <kannan@jobuli.com>
2025-10-11 22:37:26 +0530

    11.10.2025
    2-20-cd completed

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,8 +1,9 @@
 /* eslint-disable prettier/prettier */
-import { Controller, Get, Param, ParseIntPipe, UseGuards, UseInterceptors } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe, Query, UseGuards, UseInterceptors } from '@nestjs/common';
 import { PostService } from './post.service';
 import { AuthGuard } from 'src/guards/auth.guard';
 import { LoggingInterceptor } from 'src/interceptors/logging.interceptor';
+import { User } from 'src/decorators/user.decorator';
 
 @Controller()
 @UseGuards(AuthGuard)
@@ -10,7 +11,14 @@ import { LoggingInterceptor } from 'src/interceptors/logging.interceptor';
 export class PostController {
     constructor(private postService: PostService) { }
     @Get('/posts')
-    getAll() {
+    getAll(
+        @Query('page', ParseIntPipe) page: number,
+        @Query('limit', ParseIntPipe) limit: number,
+        @User() user: any
+        ) {
+        console.log(page);
+        console.log(limit);
+        console.log(user);
         return this.postService.getAll();
     }
 
