96201553c5c74884039ebc6ec040ab98228ac5b4
Jobulikannan <kannan@jobuli.com>
2025-10-12 18:54:50 +0530

    12.10.2025
    3-27-ir completed

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { Controller, Post } from '@nestjs/common';
+import { PostService } from './post.service';
 
-@Controller('post')
-export class PostController {}
+@Controller('posts') // REST
+export class PostController {
+    constructor(private readonly postService: PostService) { }
+
+    @Post() // posts/abc
+    create() {
+        return this.postService.create();
+    }
+}
--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,5 +1,19 @@
 /* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Post } from './schemas/post.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class PostService { }
+export class PostService {
+    constructor(@InjectModel(Post.name) private postModel: Model<Post>) { }
+
+    create() {
+        const createdPost = new this.postModel({
+            title: 'Post 1',
+            description: 'Description 1',
+        });
+
+        return createdPost.save();
+    }
+}
