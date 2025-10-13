163dcb6fd97553a06896aea7d73e76216065dbee
Jobulikannan <kannan@jobuli.com>
2025-10-13 22:16:31 +0530

    13.10.2025
    3-30-mad completed

---
+++ src/post/dtos/response-post.dto.ts
@@ -0,0 +1,6 @@
+/* eslint-disable prettier/prettier */
+export class ResponsePostDto {
+    _id: string;
+    title: string;
+    description: string;
+}
--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -4,14 +4,23 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Post } from './schemas/post.schema';
 import { Model } from 'mongoose';
 import { CreatePostDto } from './dtos/create-post.dto';
+import { ResponsePostDto } from './dtos/response-post.dto';
 
 @Injectable()
 export class PostService {
     constructor(@InjectModel(Post.name) private postModel: Model<Post>) { }
 
-    create(data: CreatePostDto) {
+    async create(data: CreatePostDto): Promise<ResponsePostDto> {
         const createdPost = new this.postModel(data);
 
-        return createdPost.save();
+        const post = await createdPost.save();
+
+        const postDto = new ResponsePostDto();
+
+        postDto._id = post._id.toString();
+        postDto.title = post.title;
+        postDto.description = post.description;
+
+        return postDto;
     }
 }
